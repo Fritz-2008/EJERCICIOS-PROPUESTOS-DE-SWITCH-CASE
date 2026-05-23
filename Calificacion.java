67630e6637d20c05419e4260a28f4482b2ea74ad
Fritz-2008 <fritzanimaciones@gmail.com>
2026-05-23 16:13:57 -0500

    Create Calificacion.java

---
+++ Calificacion.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Calificacion {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese letra: ");
+        char c = Character.toUpperCase(sc.next().charAt(0));
+
+        switch (c) {
+            case 'A': System.out.println("Excelente"); break;
+            case 'B': System.out.println("Bueno"); break;
+            case 'C': System.out.println("Regular"); break;
+            case 'D': System.out.println("Deficiente"); break;
+            case 'F': System.out.println("Reprobado"); break;
+            default: System.out.println("Inválido");
+        }
+    }
+}
